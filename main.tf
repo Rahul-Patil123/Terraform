9aef653144ca04a966b3639c1d7b3742afa25a90
Rahul-Patil123 <rahulpatilccis@gmail.com>
2025-08-14 22:49:51 +0530

    Update terraform file

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
 provider "aws"{
     region = "ap-south-1"
+    access_key = my_access_key
+    secret_key = my_secret_key
+}
+resource "aws_vpc" "development" {
+    cidr_block = "10.0.0.0/16"
 }
