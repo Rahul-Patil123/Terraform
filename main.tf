4f7c56fc62c2b25bd8e748b4b41a254a0e4545fc
Rahul-Patil123 <rahulpatilccis@gmail.com>
2025-08-17 19:39:46 +0530

    Add Main gateway

--- main.tf
+++ main.tf
@@ -30,8 +30,29 @@ resource "aws_subnet" "myapp_subnet_1" {
     }
 }
 
-resource "aws_route_table" "myapp-route-table" {
-    vpc_id = aws_vpc.myapp-vpc.id
+# resource "aws_route_table" "myapp-route-table" {
+#     vpc_id = aws_vpc.myapp-vpc.id
+#     route{
+#         cidr_block = "0.0.0.0/0"
+#         gateway_id = aws_internet_gateway.myapp-igw.id
+#     }
+#     tags = {
+#         Name: "${var.env_prefix}-rtb"
+#     }
+# }
+# resource "aws_route_table_associations" "a-rtb" {
+#     subnet_id = aws_subnet.myapp_subnet_1.id
+#     route_table_id = aws_route_table.myapp-route-table.id
+# }
+
+# data "aws_vpc" "existing_vpc"{
+#     default = true
+# }
+# output "dev-vpc-id" {
+#   value = "data.aws_vpc.existing_vpc.id"
+# }
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
     route{
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.myapp-igw.id
@@ -40,20 +61,9 @@ resource "aws_route_table" "myapp-route-table" {
         Name: "${var.env_prefix}-rtb"
     }
 }
-
 resource "aws_internet_gateway" "myapp-igw" {
     vpc_id = aws_vpc.myapp-vpc.id
     tags = {
         Name: "${var.env_prefix}-igw"
     }
 }
-resource "aws_route_table_associations" "a-rtb" {
-    subnet_id = aws_subnet.myapp_subnet_1.id
-    route_table_id = aws_route_table.myapp-route-table.id
-}
-# data "aws_vpc" "existing_vpc"{
-#     default = true
-# }
-# output "dev-vpc-id" {
-#   value = "data.aws_vpc.existing_vpc.id"
-# }
