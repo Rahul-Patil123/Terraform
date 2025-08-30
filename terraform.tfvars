4a28b931f937eab195d4b52aabdc9dbbb8e69e95
Rahul-Patil123 <rahulpatilccis@gmail.com>
2025-08-31 01:40:40 +0530

    Add vpc file

--- terraform.tfvars
+++ terraform.tfvars
@@ -6,3 +6,6 @@ my_ip = "172.22.192.1/32"
 instance_type = "t2.micro"
 my_public_key_location = "/Users/Rahul/.shh/id_rsa.pub"
 private_key_location = "/Users/Rahul/.shh/id_rsa"
+vpc_cidr_block = "10.0.0.0/16"
+private_subnet_cidr_blocks = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+public_subnet_cidr_blocks = ["10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+provider "aws"{
+    region = "ap-south-1"
+}
 variable vpc_cidr_block {}
 variable private_subnet_cidr_blocks {}
 variable public_subnet_cidr_blocks {}
+
+data "aws_availiblity_zones" "azs" {}
 
 module "myapp-vpc" {
     source = "terraform-aws-modules/vpc/aws"
@@ -10,4 +23,14 @@ module "myapp-vpc" {
     # Best practice is one public and one private subnet in each availibility zone
     private_subnets = var.private_subnet_cidr_blocks
     public_subnets = var.public_subnet_cidr_blocks
+    azs = data.aws_availiblity_zones.azs.names
+
+    enable_nat_gateway = true
+    single_nat_gateway = true
+    enable_dns_hostname = true
+
+    tags {
+        "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    }
+
 }
