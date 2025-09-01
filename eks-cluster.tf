a0cda962f98b5eae66c0630ffed2e2cbad7622f1
Rahul-Patil123 <rahulpatilccis@gmail.com>
2025-09-01 22:56:53 +0530

    create eks cluster

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,4 +1,38 @@
+provider "kubernetes" {
+    load_config_file = false 
+    host = data.aws_eks_cluster.myapp-cluster.endpoint
+    token = data.aws_eks_cluster_auth.myapp-cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp-cluster.certificate_authority.0.data)
+}
+data "aws_eks_cluster" "myapp-cluster" {
+    name = module.eks.cluster_id
+}
+data "aws_eks_cluster_auth" "myapp-cluster" {
+    name = module.eks.cluster_id
+
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.1.5"
+
+  name = "myapp-eks-cluster"
+
+  subnet_ids = module.myapp-vpc.private_subnets
+  vpc_id = module.myapp-vpc.vpc_id
+  tags = {
+    environment = "development"
+    application = "myapp"
+  }
+  eks_managed_node_groups = {
+    worker_group_1 = {
+      desired_capacity = 3
+      instance_types   = ["t2.small"]
+      # key_name = "my-key"   # optional if you have an SSH key
+    }
+    worker_group_2 = {
+      desired_capacity = 2
+      instance_types   = ["t2.medium"]
+    }
+  }
 }
--- vpc.tf
+++ vpc.tf
@@ -12,7 +12,7 @@ provider "aws"{
 variable private_subnet_cidr_blocks {}
 variable public_subnet_cidr_blocks {}
 
-data "aws_availiblity_zones" "azs" {}
+data "aws_availability_zones" "azs" {}
 
 module "myapp-vpc" {
     source = "terraform-aws-modules/vpc/aws"
@@ -22,11 +22,11 @@ module "myapp-vpc" {
     # Best practice is one public and one private subnet in each availibility zone
     private_subnets = var.private_subnet_cidr_blocks
     public_subnets = var.public_subnet_cidr_blocks
-    azs = data.aws_availiblity_zones.azs.names
+    azs = data.aws_availability_zones.azs.names
 
     enable_nat_gateway = true
     single_nat_gateway = true
-    enable_dns_hostname = true
+    enable_dns_hostnames = true
 #  We are using this tags to tell CCM Cloud Control Manager to communicate with resources belonging to same group of resources
     tags = {
         "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
